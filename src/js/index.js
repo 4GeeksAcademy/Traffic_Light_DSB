e6981e661ac84351d63935008da77099597c84f0
DiegoSB9 <diego_sb_95@hotmail.com>
2024-12-09 11:34:06 +0000

    first commit1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TrafficLight from "./component/TrafficLight.jsx";
-import "../styles/index.css"; // Importa el archivo CSS global
+import "../styles/index.css";
 
 ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
